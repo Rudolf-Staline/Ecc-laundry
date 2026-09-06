95460e1814ee6c78f41184ce7c47d62109c19895
Rudolf Hounlete <koffi.hounlete@imsp-uac.org>
2026-09-06 09:09:30 +0100

    Make global laundry background animations denser

--- app/layout.tsx
+++ app/layout.tsx
@@ -61,7 +61,7 @@ const SCRIPT_THEME = `
 })();
 `;
 
-const BULLES_GLOBALES = [1, 3, 5, 7, 9, 11, 13, 14];
+const BULLES_GLOBALES = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14];
 
 function AmbianceGlobale() {
   return (
